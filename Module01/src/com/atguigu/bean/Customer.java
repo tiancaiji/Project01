25a26abd5ac8058cf1b9c0e8c8545e4ba8d1335f
yxj <1207166462@qq.com>
2020-04-04 20:36:02 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,10 @@
+package com.atguigu.bean;
+
+//这是单行注释
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+    }
+
+
+}
---
+++ Module01/src/com/atguigu/bean/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @author Yxj
+ * @create 2020-04-04 12:49
+ */
+public class Hello {
+    public static void main(String[] args) {
+
+    }
+}
+
+
